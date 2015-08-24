e6445993482890d853ded78f3ee463f1ed12bcbe
wcxdell <wcxdell@163.com>
2015-08-24 22:47:18 +0800

    创建

---
+++ calculator/ViewController.h
@@ -0,0 +1,26 @@
+//
+//  ViewController.h
+//  calculator
+//
+//  Created by 王长旭 on 15/8/24.
+//  Copyright (c) 2015年 王长旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *base;
+@property (strong, nonatomic) IBOutlet UILabel *answor;
+@property (strong, nonatomic) IBOutlet UIButton *btn1;
+@property (strong, nonatomic) IBOutlet UIButton *btn2;
+@property (strong, nonatomic) IBOutlet UIButton *btn3;
+@property (strong, nonatomic) IBOutlet UIButton *btn4;
+@property (strong, nonatomic) IBOutlet UIButton *btn5;
+
+@property (strong, nonatomic) IBOutlet UIButton *btn6;
+@property (strong, nonatomic) IBOutlet UIButton *btn7;
+@property (strong, nonatomic) IBOutlet UIButton *btn8;
+@property (strong, nonatomic) IBOutlet UIButton *btn9;
+
+@end
+
